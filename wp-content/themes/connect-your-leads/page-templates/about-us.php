0d725e0c83df5aa4f1326e0dabbf7a8ab908cd1d
hathbanger <hathbanger.ah@gmail.com>
2016-08-03 12:00:30 -0600

    about us stuff

--- wp-content/themes/connect-your-leads/page-templates/about-us.php
+++ wp-content/themes/connect-your-leads/page-templates/about-us.php
@@ -41,7 +41,8 @@
                 </h2>
             <div class="col-md-12 partner-section">
                 <div class="col-md-6" style="padding: 100px;">
-                	<img src="<?php echo get_template_directory_uri(); ?>/images/cyh_logo.png">
+                <?php $first_image = get_field('first_image');?>
+                	<img src="<?php echo $first_image['url']; ?>">
                 </div>
                 <div class="col-md-6">
                     <h4 class="home-align">
@@ -66,23 +67,43 @@
                     </p>
                 </div>
                 <div class="col-md-6">
-                	<img src="<?php echo get_template_directory_uri(); ?>/images/callcenter1.1.jpg">
+                <?php $second_image = get_field('second_image');?>
+                    <img src="<?php echo $second_image['url']; ?>">
                 </div>
 
 
             </div>
         </div>
 
+        <div class="row service-modules">
+            <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-md-6">
+                            <a href="/buy-leads">
+                        <div class="service buy-leads">
+                                <div class="textHolder">
+                                    <h2>
+                                        BUY<br/>LEADS<br/>HERE
+                                    </h2>
+                                </div>
+                        </div>
+                            </a>
+                    </div>   
+                    <div class="col-md-6">                  
+                        <a href="/sell-leads">
+                            <div class="service sell-leads">
+                                     <div class="textHolder">
+                                        <h2>
+                                            SELL<br/>LEADS<br/>HERE
+                                        </h2>
+                                    </div>
+                            </div>
+                        </a>
+                    </div>
 
-        <div class="row">
-            <div class="col-md-12">
-                <div class="masthead">
-                    <?php $hero_mid_image = get_field('about_us_mid_hero');?>
-                    <img src="<?php echo $hero_mid_image['url']; ?>" alt="<?php echo $hero_mid_image['alt'] ?>" />
                 </div>
             </div>
-            <!-- /.col-md-12 -->
-        </div>        
+        </div>  
 
 
     </section>
