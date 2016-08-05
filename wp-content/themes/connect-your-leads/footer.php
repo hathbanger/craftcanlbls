4a9fdb49b3698d7dae11c7139ecd9801401cb4c6
hathbanger <hathbanger.ah@gmail.com>
2016-08-05 10:28:51 -0600

    added livechat

--- wp-content/themes/connect-your-leads/footer.php
+++ wp-content/themes/connect-your-leads/footer.php
@@ -37,10 +37,22 @@
     <script src="<?php echo get_template_directory_uri(); ?>/javascripts/bootstrap.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/javascripts/fastclick.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/javascripts/jvfloat.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/javascripts/main.js"></script>
+<!--     <script src="<?php echo get_template_directory_uri(); ?>/javascripts/main.js"></script> -->
 <!--     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTUh9CBBaNmVbno_LaDXlpDFnbGUg9rPA&signed_in=true&libraries=places&callback=initAutocomplete" async defer></script> -->
 
-  <script src="https://code.jquery.com/ui/1.10.3/jquery-ui.min.js"></script>    
+<!-- Start of LiveChat (www.livechatinc.com) code -->
+<script type="text/javascript">
+window.__lc = window.__lc || {};
+window.__lc.license = 8068871;
+(function() {
+  var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+  lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+})();
+</script>
+<!-- End of LiveChat code -->
+
+  <!-- <script src="https://code.jquery.com/ui/1.10.3/jquery-ui.min.js"></script>     -->
 <?php wp_footer(); ?>
 
 </body>
