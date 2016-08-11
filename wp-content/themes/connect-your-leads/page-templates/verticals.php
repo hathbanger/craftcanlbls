4c71c8cd94c24485da3d8bb30ffde911dab24032
hathbanger <hathbanger.ah@gmail.com>
2016-08-11 09:22:47 -0600

    added hr above services

--- wp-content/themes/connect-your-leads/page-templates/verticals.php
+++ wp-content/themes/connect-your-leads/page-templates/verticals.php
@@ -46,6 +46,7 @@
         </div>
         
 
+        <hr/>
         <div class="row">
                 <div class="col-md-12">
                     <?php if (have_posts()) : while (have_posts()) : the_post();?>
