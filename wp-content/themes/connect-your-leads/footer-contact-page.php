2c5a3782b7ae4ad2312ef13f3dda2afc1b3f2fa6
hathbanger <hathbanger.ah@gmail.com>
2016-08-18 09:16:41 -0600

    removed google analytics

--- wp-content/themes/connect-your-leads/footer-contact-page.php
+++ wp-content/themes/connect-your-leads/footer-contact-page.php
@@ -57,18 +57,7 @@
     <script src="<?php echo get_template_directory_uri(); ?>/javascripts/fastclick.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/javascripts/jvfloat.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/javascripts/main.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTUh9CBBaNmVbno_LaDXlpDFnbGUg9rPA&signed_in=true&libraries=places&callback=initAutocomplete" async defer></script>
 
 <?php wp_footer(); ?>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-18407193-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 </body>
 </html>
