652b40ff22b67e846b9551205ddc19387e1a39b1
hathbanger <hathbanger.ah@gmail.com>
2016-08-18 15:17:14 -0600

    added google analytics

--- wp-content/themes/connect-your-leads/footer-contact-page.php
+++ wp-content/themes/connect-your-leads/footer-contact-page.php
@@ -57,7 +57,14 @@
     <script src="<?php echo get_template_directory_uri(); ?>/javascripts/fastclick.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/javascripts/jvfloat.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/javascripts/main.js"></script>
-
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-18407193-3', 'auto');
+  ga('send', 'pageview');
+</script>
 <?php wp_footer(); ?>
 </body>
 </html>
